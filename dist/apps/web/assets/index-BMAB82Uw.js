000c7cf9ff39561f37fcb2886c2e56c80d87d0bc
vonKraus <vonkraus01@vonKraus-Mac-Studio.local>
2026-06-09 10:55:02 -0400

    remove dist from tracking, update gitignore

--- dist/apps/web/assets/index-BMAB82Uw.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D0K10Hry.js","assets/index-DFpCa_aP.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./index-nqHzW3Ai.js";import{r as o}from"./index-DFpCa_aP.js";const i=o("Share",{web:()=>e(()=>import("./web-D0K10Hry.js"),__vite__mapDeps([0,1])).then(r=>new r.ShareWeb)});export{i as Share};
